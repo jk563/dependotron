2f611ac330a4e3f62b81105acc4504a66cff519a
roberton <rnerici@gmail.com>
2013-12-18 15:56:20 +0000

    Updated main file to match our latest thinking on the design.

--- pomanalyser.py
+++ pomanalyser.py
@@ -6,6 +6,18 @@ Does not depend on!
     - SVN: we get given the contents of the POM directly, regardless of where it came from
 """
 import unittest
+import tempfile
+from subprocess import call
+
+
+class ArtifactDependencyInfo:
+    """
+    Simple wrapper around the following directory.
+    ((name, version), [(name, version, direct), ...)])
+    """
+    def __init__(self, artifactInfo, dependencies):
+        self.artifactInfo = artifactInfo
+        self.dependencies = dependencies
 
 
 class PomAnalyser:
@@ -16,9 +28,24 @@ class PomAnalyser:
         """
         Analyses the contents of [pom] and returns a list of tuples of dependencies.
         Uses maven to do the analysis.
-        Q: can it take the contents of a POM or does it need to fetch it?
+        Returns an ArtifactDependencyInfo
         """
         print "pomanalyser: hi! %d" % use_direct_only
+        pomTemporaryPath = self._save_pom_contents_to_temporary_path(pomContents)
+        self._do_some_maven_magic(pomTemporaryPath)
+        return ArtifactDependencyInfo("", [])
+
+    def _save_pom_contents_to_temporary_path(self, pomContents):
+        temporaryPathAndFileName = tempfile.NamedTemporaryFile().name
+        tempFile = open(temporaryPathAndFileName, "w")
+        tempFile.writelines(pomContents)
+        return temporaryPathAndFileName
+
+    def _do_some_maven_magic(self, pomTemporaryPath):
+        print ">>> _do_some_maven_magic called with", pomTemporaryPath
+        call(["ls", "-l"])
+
+
 
 
 # If run as a program then run unit tests
@@ -26,4 +53,6 @@ if __name__ == "__main__":
     pomFile = open("test/resources/simple_pom/pom.xml")
     pomContents = pomFile.readlines()
     analyser = PomAnalyser()
-    analyser.analyse(pomContents)
+
+    dependencyInfo = analyser.analyse(pomContents)
+    print dependencyInfo
