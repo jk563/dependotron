4331b626ffe14311cc69a0f9480fb34dd20d1140
akatank <jk563htc@gmail.com>
2013-12-18 14:07:44 +0000

    Basic schema generator

--- schemaGenerator.py
+++ schemaGenerator.py
@@ -1,6 +1,32 @@
-class SchemaGenerator:
-    def generateSchema(self):
-      databaseConnection = getDatabaseConnection()
+import MySQLdb
 
-    def getDatabaseConnection(self):
-        return None
+
+class SchemaGenerator:
+    def generateSchema(self, host,user,password,database):
+        mySQLConnection = MySQLdb.connect(host=host, user=user, passwd=password)
+        self.createDependotronDatabaseIfExists(mySQLConnection)
+        dependotronConnection = MySQLdb.connect(host=host, user=user, passwd=password, db=database)
+        self.dependotronCursor = dependotronConnection.cursor()
+        self.createTable()
+
+    def setMySQLConnection(self,host,user,password):
+        self.mySQLConnection = MySQLdb.connect(host=host, user=user, passwd=password)
+
+    def createDependotronDatabaseIfExists(self, mySQLConnection):
+        if(mySQLConnection):
+            mySqlCursor = mySQLConnection.cursor()
+            createDependotronSQL = "CREATE DATABASE IF NOT EXISTS dependotron;"
+            mySqlCursor.execute(createDependotronSQL)
+        else:
+            raise Exception("No MySQL connection set.")
+
+    def createTable(self):
+        if(self.dependotronCursor):
+            createDependotronSQL = "CREATE TABLE IF NOT EXISTS dependencies (dependantName VARCHAR(255), dependantVersion VARCHAR(15), dependencyName VARCHAR(255), dependencyVersion VARCHAR(15), directDependency BOOLEAN);"
+            self.dependotronCursor.execute(createDependotronSQL)
+        else:
+            raise Exception("No dependotron connection set.")
+
+if __name__ == '__main__':
+    gen = SchemaGenerator()
+    gen.generateSchema('localhost','root','','dependotron')
