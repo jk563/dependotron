70a36dd40d121539ed184f43230ec996a72ecd15
akatank <jk563htc@gmail.com>
2014-01-01 14:51:21 +0000

    Test framework for Database class.

--- test/database_test.py
+++ test/database_test.py
@@ -1,1 +1,9 @@
-__author__ = 'kellyj29'
+import unittest
+import database
+
+class DatabaseTest(unittest.TestCase):
+    def first_test(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
