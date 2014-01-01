a3753842a5f632bbd429f71ac6abfe0c18ebb094
akatank <jk563htc@gmail.com>
2014-01-01 14:54:49 +0000

    Database test name change.

--- test/database_test.py
+++ test/database_test.py
@@ -2,7 +2,8 @@ import unittest
 import database
 
 class DatabaseTest(unittest.TestCase):
-    def first_test(self):
+
+    def add_adds_dependencies_to_database(self):
         pass
 
 if __name__ == '__main__':
