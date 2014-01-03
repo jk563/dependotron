45b5e54f2e9c6a6813b3742b9e36f4108be24e90
akatank <jk563htc@gmail.com>
2014-01-03 20:36:06 +0000

    Made connection method private.

--- databaseinitialiser.py
+++ databaseinitialiser.py
@@ -6,11 +6,11 @@ class DatabaseInitialiser:
         self.user = user
         self.passwd = passwd
 
-    def connect_to_database(self):
-        self.connection = MySQLdb.connect(host=self.host, user=self.user, passwd=self.passwd)
-
     def initialise_database(self, database):
         cursor = self.connection.cursor()
         create_database_sql = 'CREATE DATABASE %s;' % (database)
         cursor.execute(create_database_sql)
         self.connection.commit()
+
+    def _connect_to_database(self):
+        self.connection = MySQLdb.connect(host=self.host, user=self.user, passwd=self.passwd)
