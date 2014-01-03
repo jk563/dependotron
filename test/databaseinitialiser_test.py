60766c9b99562ff19bdedf363e2fc9a75b55ce3d
akatank <jk563htc@gmail.com>
2014-01-03 19:46:07 +0000

    Test to check database is created if it doesn't already exist.

--- test/databaseinitialiser_test.py
+++ test/databaseinitialiser_test.py
@@ -1,15 +1,31 @@
 import unittest
+import MySQLdb
 import databaseinitialiser
 
 class DatabaseInitialiserTest(unittest.TestCase):
     def setUp(self):
-      host = 'localhost'
-      user = 'root'
-      passwd = ''
-      self.database_initialiser = databaseinitialiser.DatabaseInitialiser(host, user, passwd)
+        self.host = 'localhost'
+        self.user = 'root'
+        self.passwd = ''
+        self.database_initialiser = databaseinitialiser.DatabaseInitialiser(self.host, self.user, self.passwd)
 
     def test_connection_can_be_made(self):
-      self.database_initialiser.connect_to_database()
-      self.assertTrue(self.database_initialiser.connection)
+        self.database_initialiser.connect_to_database()
+        self.assertTrue(self.database_initialiser.connection)
 
-    
+    def test_database_created_if_it_does_not_exist(self):
+        self.database_initialiser.connect_to_database()
+        self.database_initialiser.initialise_database('database_initialiser_test')
+        self.assertTrue(self._does_database_exist('dependotron-test'), 'Database does not exist.')
+
+    def _does_database_exist(self, database_name):
+        connection = MySQLdb.connect(host=self.host, user=self.user, passwd=self.passwd)
+        cursor = connection.cursor()
+        database_exists_query = "SELECT COUNT(SCHEMA_NAME) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='%s'" \
+                                % (database_name)
+        cursor.execute(database_exists_query)
+        number_of_results = cursor.fetchone()
+        if number_of_results == 1:
+            return True
+        else:
+            return False
